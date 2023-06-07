f7ac4a4e469375e59d6b3771b97a69036369c322
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-06-07 04:03:25 -0400

    Compose Article - Done

--- LearnTogether/app/src/main/java/com/example/learntogether/MainActivity.kt
+++ LearnTogether/app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +32,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HeaderImage()
+                    Column(
+                        verticalArrangement = Arrangement.Top
+                    ) {
+                        HeaderImage()
+                        TitleText()
+                        BodyText()
+                    }
                 }
             }
         }
@@ -37,6 +51,32 @@ fun HeaderImage() {
     Image(painter = painter, contentDescription = null, contentScale = ContentScale.FillWidth)
 }
 
+@Composable
+fun TitleText() {
+    Text(
+        text = stringResource(R.string.title),
+        fontSize = 24.sp,
+        modifier = Modifier.padding(16.dp)
+    )
+}
+
+@Composable
+fun BodyText() {
+        Text(
+            text = stringResource(R.string.body1),
+            modifier = Modifier.padding(
+                start = 16.dp,
+                end = 16.dp
+            ),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = stringResource(R.string.body2),
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+}
+
 @Preview(
     showBackground = true,
     showSystemUi = true
@@ -45,5 +85,7 @@ fun HeaderImage() {
 fun GreetingPreview() {
     LearnTogetherTheme {
         HeaderImage()
+        TitleText()
+        BodyText()
     }
 }
