351c17ace73e68aa811ec435df7579da7641f61e
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-06-07 03:15:30 -0400

    first build

--- LearnTogether/app/src/main/java/com/example/learntogether/MainActivity.kt
+++ LearnTogether/app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -30,6 +30,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun HeaderImage() {
+    
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
