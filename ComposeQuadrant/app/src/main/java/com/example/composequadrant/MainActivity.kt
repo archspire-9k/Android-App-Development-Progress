c09cd2d0ab54905769c5cfaeca1b9b5c2239cd69
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-06-07 04:51:24 -0400

    center aligned Quadrant

--- ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.composequadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
@@ -37,10 +40,28 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Quadrant(title : String, body: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = title,
+            modifier = modifier
+        )
+        Text(
+            text = body,
+            modifier = modifier
+        )
+    }
+
+}
+
+@Preview(showBackground = true, showSystemUi = false)
+@Composable
+fun QuadrantPreview() {
     ComposeQuadrantTheme {
-        Greeting("Android")
+        Quadrant("title", "body")
     }
 }
