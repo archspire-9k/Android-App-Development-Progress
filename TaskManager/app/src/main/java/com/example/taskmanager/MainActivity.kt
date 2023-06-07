e8983322c1e248b5ad38ac5cfa5935928ba05a8d
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-06-07 04:40:39 -0400

    extract strings - DONE

--- TaskManager/app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ TaskManager/app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -47,7 +48,7 @@ fun Layout() {
     ) {
         Image(painter = painter, contentDescription = null)
         Text(
-            text = "All task completed",
+            text = stringResource(R.string.all_task_completed),
             fontWeight = FontWeight.Bold,
             modifier = Modifier
                 .padding(
@@ -56,7 +57,7 @@ fun Layout() {
                 )
         )
         Text(
-            text = "Nice work!",
+            text = stringResource(R.string.nice_work),
             fontSize = 16.sp
         )
     }
