602f20e8de8756bd4b9158f23e99ad3277210bfc
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-06-08 00:52:07 -0400

    move from preview to app render

--- BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
+++ BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,7 +50,8 @@ class MainActivity : ComponentActivity() {
                     ) {
                         val weight = Modifier.weight(1f)
                         ProfileComponent(Modifier.weight(2f))
-                        ContactInfo("123", "github.com", "email@sample.com", weight)
+                        ContactInfo(stringResource(R.string.phone_number), stringResource(R.string.link), stringResource(
+                            R.string.email), weight)
                     }
                 }
             }
@@ -125,7 +127,8 @@ fun GreetingPreview() {
         ) {
             val weight = Modifier.weight(1f)
             ProfileComponent(Modifier.weight(2f))
-            ContactInfo("+1 234 931 9865", "github.com/john_doe", "email@sample.com", weight)
+            ContactInfo(stringResource(R.string.phone_number), stringResource(R.string.link), stringResource(
+                            R.string.email), weight)
         }
     }
 }
