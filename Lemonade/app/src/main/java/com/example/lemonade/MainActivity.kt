708fd1d45a051a349ac66181779a9665d951820c
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-06-08 23:04:46 -0400

    added scaffold, implement squeeze logic

--- Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.lemonade
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,7 +35,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,14 +43,24 @@ class MainActivity : ComponentActivity() {
         setContent {
             LemonadeTheme {
                 // A surface container using the 'background' color from the theme
-                    LemonApp()
+                LemonApp()
             }
         }
     }
 }
 
 @Composable
-fun ImageAndText(textResource: String, imageResource: Int, onClick: () -> Unit, modifier: Modifier = Modifier) {
+fun ImageAndText(
+    textResource: String,
+    imageResource: Int,
+    step: Int,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var count = 0
+    if(step == 2) {
+        count = (2..4).random()
+    }
 
     Column(
         modifier = modifier,
@@ -52,14 +68,16 @@ fun ImageAndText(textResource: String, imageResource: Int, onClick: () -> Unit, 
         horizontalAlignment = Alignment.CenterHorizontally
     )
     {
-        Text(
-            text = "Lemonade",
-            fontSize = 18.sp,
-            color = Color.Black,
-            fontWeight = FontWeight.Bold
-        )
         Button(
-            onClick = onClick,
+            onClick = {
+                    if (count > 0) {
+                        count -= 1
+                        Log.d("SQUEEZE COUNT", "$count")
+                    } else {
+                        onClick()
+                    }
+                }
+            ,
             shape = RoundedCornerShape(10.dp),
             colors = ButtonDefaults.buttonColors(containerColor = Color(0xffd2e7d4))
         ) {
@@ -73,6 +91,7 @@ fun ImageAndText(textResource: String, imageResource: Int, onClick: () -> Unit, 
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun LemonApp() {
@@ -91,22 +110,45 @@ fun LemonApp() {
         else -> stringResource(R.string.lemon_restart)
     }
 
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        ImageAndText(
-            textResource,
-            imageResource,
-            {
-                if( step in 1..3 ) {
-                    step ++
-                } else {
-                    step = 1
-                }
-            },
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Lemonade",
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                    actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            )
+        }
+    ) { innerPadding ->
+        Surface(
             modifier = Modifier
                 .fillMaxSize()
-        )
+                .padding(innerPadding)
+                .background(MaterialTheme.colorScheme.tertiaryContainer),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            ImageAndText(
+                textResource,
+                imageResource,
+                step,
+                {
+                    if (step in 1..3) {
+                        step++
+                    } else {
+                        step = 1
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+        }
     }
 }
