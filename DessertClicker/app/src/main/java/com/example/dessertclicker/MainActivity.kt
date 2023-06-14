7431760b1c765bd72f39c2dbbbf7c930706057ab
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-06-14 04:15:55 -0400

    Clear warnings

--- DessertClicker/app/src/main/java/com/example/dessertclicker/MainActivity.kt
+++ DessertClicker/app/src/main/java/com/example/dessertclicker/MainActivity.kt
@@ -39,16 +39,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Share
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -168,7 +167,7 @@ private fun DessertClickerApp(
 ) {
 
     var revenue by rememberSaveable { mutableStateOf(0) }
-    var dessertsSold by remember { mutableStateOf(0) }
+    var dessertsSold by rememberSaveable { mutableStateOf(0) }
 
     val currentDessertIndex by rememberSaveable { mutableStateOf(0) }
 
