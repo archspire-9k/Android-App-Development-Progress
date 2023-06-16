11b2ddcd314cd2699a1571ffa3a7179b018c454f
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-06-15 22:34:07 -0400

    Add Canvas into ComposeView

--- CameraXStarter/app/src/main/java/com/example/cameraxstarter/CameraView.kt
+++ CameraXStarter/app/src/main/java/com/example/cameraxstarter/CameraView.kt
@@ -1,40 +1,25 @@
 package com.example.cameraxstarter
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cameraxstarter.ui.theme.CameraXStarterTheme
 
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun CameraView(modifier: Modifier = Modifier) {
 
             CameraXStarterTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = Color.Transparent
-                ) {
-                    Greeting("Android", modifier)
+                Canvas(modifier = Modifier.fillMaxSize()) {
+                    val canvasQuadrantSize = size / 2F
+                    drawRect(
+                        color = Color.Magenta,
+                        size = canvasQuadrantSize
+                    )
                 }
             }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun GreetingPreview() {
-    CameraXStarterTheme {
-        Greeting("Android")
-    }
 }
