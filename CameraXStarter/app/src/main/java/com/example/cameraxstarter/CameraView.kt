7d88ea6d751a31efd0368b3ab61b57eda88a62df
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-06-15 22:30:04 -0400

    Change ComposeView background to transparent

--- CameraXStarter/app/src/main/java/com/example/cameraxstarter/CameraView.kt
+++ CameraXStarter/app/src/main/java/com/example/cameraxstarter/CameraView.kt
@@ -1,11 +1,11 @@
 package com.example.cameraxstarter
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cameraxstarter.ui.theme.CameraXStarterTheme
 
@@ -16,7 +16,7 @@ fun CameraView(modifier: Modifier = Modifier) {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.Transparent
                 ) {
                     Greeting("Android", modifier)
                 }
@@ -31,7 +31,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     CameraXStarterTheme {
